997cdbd53036b2fa1483b67a08b4c05c4da30c49
Ryan Hunt <rhunt@eqrion.net>
2017-07-23 10:26:40 -0500

    Add scripts for installing firefox nightly

---
+++ firefox-nightly/update-nightly.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+RELEASE=56.0a1
+TARBALL="firefox-$RELEASE.en-US.linux-x86_64.tar.bz2"
+
+wget https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central/$TARBALL
+
+if [ -d "/opt/firefox" ]; then
+  rm -r /opt/firefox
+  mkdir /opt/firefox
+fi
+
+tar -xvjf $TARBALL -C /opt
+rm $TARBALL
+
+sudo desktop-file-install --dir=/usr/share/applications firefox-nightly.desktop
